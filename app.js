4d6bb476995604cad59609cbe9e388c2bc9d9f53
Salomaoft <161130744+Salomaoft@users.noreply.github.com>
2024-04-27 23:08:32 -0300

    Update app.js

--- app.js
+++ app.js
@@ -21,8 +21,8 @@ function verificarChute(){
     
     if(chute == numeroSecreto){
         exibirTextoNaTela("h1", "Acertou!");
-        let palavraTentativa = tentativas > 1 ? "tentativas" : tentativa;
-        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa} tentativas`;
+        let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa";
+        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
         exibirTextoNaTela("p", mensagemTentativas);
         document.getElementById("reiniciar").removeAttribute("disabled")
     }else {
